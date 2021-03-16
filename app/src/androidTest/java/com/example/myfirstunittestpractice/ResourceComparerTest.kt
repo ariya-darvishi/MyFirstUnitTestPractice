13a23176a4deb2f00f29cddfaf2c272fef1bc386
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-16 12:31:51 +0330

    Writing tests for ResourceComparer class  :)

---
+++ app/src/androidTest/java/com/example/myfirstunittestpractice/ResourceComparerTest.kt
@@ -0,0 +1,33 @@
+package com.example.myfirstunittestpractice
+
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest{
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup(){
+        resourceComparer = ResourceComparer()
+    }
+
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "MyFirstUnitTestPractice")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "MyFirstUnitTest")
+        assertThat(result).isFalse()
+    }
+}
