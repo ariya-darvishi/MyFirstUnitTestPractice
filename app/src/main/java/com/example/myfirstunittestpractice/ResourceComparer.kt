f7c823643502f806b1331bf6a003253293964268
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-16 10:50:55 +0330

    Setup Resource Comparer class for testing :)

---
+++ app/src/main/java/com/example/myfirstunittestpractice/ResourceComparer.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstunittestpractice
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
+
+}
